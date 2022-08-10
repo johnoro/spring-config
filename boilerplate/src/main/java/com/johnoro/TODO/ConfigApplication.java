8e45a5cb0ac7d9663b302c3e8b6e63816f58931d
johnoro <officialjohnorourke@gmail.com>
2022-08-09 20:01:50 -0400

    Adds boilerplate code for different kinds of spring boot configurations

---
+++ boilerplate/src/main/java/com/johnoro/TODO/ConfigApplication.java
@@ -0,0 +1,34 @@
+package com.johnoro.TODO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.johnoro.TODO.model.Address;
+import com.johnoro.TODO.model.Phone;
+import com.johnoro.TODO.model.Student;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(ConfigApplication.class, args);
+
+        Address address = applicationContext.getBean(Address.class);
+        address.setCity("XYZville");
+        address.setZipCode("55555");
+        address.setState("OH");
+        address.setCountry("USA");
+
+        Phone phone = applicationContext.getBean(Phone.class);
+        phone.setMobile("7777777");
+
+        Student student = applicationContext.getBean(Student.class);
+        student.setId(0);
+        student.setName("John");
+        student.setAddress(address);
+        student.setPhones(List.of(phone));
+        System.out.println(student);
+    }
+}
